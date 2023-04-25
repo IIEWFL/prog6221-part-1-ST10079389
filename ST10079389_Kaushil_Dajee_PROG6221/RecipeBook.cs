7d2d4a317d8a7a8cbca95218f3805aae9203e405
Kaushil Dajee <104414927+ST10079389@users.noreply.github.com>
2023-04-25 20:24:59 +0200

    Added an input and output method

--- ST10079389_Kaushil_Dajee_PROG6221/RecipeBook.cs
+++ ST10079389_Kaushil_Dajee_PROG6221/RecipeBook.cs
@@ -25,12 +25,12 @@ namespace ST10079389_Kaushil_Dajee_PROG6221
             switch (option)
             {
                 case 1:
-                    //Input();
+                    Input();
                     break;
                 case 2:
                     if (opt)
                     {
-                        //Output();
+                        Output();
 
                     }
                     else
@@ -81,6 +81,94 @@ namespace ST10079389_Kaushil_Dajee_PROG6221
             Console.WriteLine("You need to input values");
             Menu_Options();
         }
+        public void Input()
+        {
+            try
+            {
+                Console.WriteLine("Enter the Recipes Name");
+                recipeName[0] = Console.ReadLine();
+                Console.WriteLine("Enter the number of ingridients for " + recipeName[0]);
+                int numberOfIngridients = Convert.ToInt32(Console.ReadLine());
+                recipeIngridients = new string[numberOfIngridients];
+                quantity = new double[numberOfIngridients];
+                originalQuantity = new double[numberOfIngridients];
+                measurementIngrident = new string[numberOfIngridients];
+                Console.WriteLine("Enter the names of the ingridients");
+                for (int i = 0; i < numberOfIngridients; i++)
+                {
+                    recipeIngridients[i] = Console.ReadLine();
+                }
+                for (int i = 0; i < numberOfIngridients; i++)
+                {
+                    Console.WriteLine("Enter the quantity of the " + recipeIngridients[i] + " and the unit of measurement (e.g. 2 table spoons)");
+                    string input = Console.ReadLine();
 
+                    string[] inputArray = input.Split(' ');
+                    if (double.TryParse(inputArray[0], out double quantityValue))
+                    {
+                        quantity[i] = quantityValue;
+                        originalQuantity[i] = quantity[i];
+                    }
+                    measurementIngrident[i] = inputArray[1];
+                }
+                Console.WriteLine("Enter the number of steps required to make " + recipeName[0]);
+                int numberOfSteps = Convert.ToInt32(Console.ReadLine());
+                steps = new string[numberOfSteps];
+                Console.WriteLine("Enter the steps reuired to make " + recipeName[0]);
+                for (int i = 0; i < numberOfSteps; i++)
+                {
+                    steps[i] = Console.ReadLine();
+                }
+                opt = true;
+                RecipeInformation blonde = new RecipeInformation(recipeName, recipeIngridients, measurementIngrident, steps, quantity, originalQuantity);
+                blonde.saveRecipes(recipeName, recipeIngridients, measurementIngrident, steps, quantity, originalQuantity);
+                Console.WriteLine("Recipe has been saved successfully");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. Please try again.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The input value is too large. Please try again.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An unexpected error occurred");
+            }
+            finally
+            {
+                Menu_Options();
+            }
+        }
+        public void Output()
+        {
+            try
+            {
+                Console.WriteLine("Recipe Name: " + recipeName[0]);
+                Console.WriteLine("Recipe Ingridients: ");
+                for (int i = 0; i < recipeIngridients.Length; i++)
+                {
+                    Console.WriteLine("-" + recipeIngridients[i]);
+                }
+                for (int i = 0; i < recipeIngridients.Length; i++)
+                {
+                    Console.WriteLine($"The quantity of {recipeIngridients[i]} is {quantity[i]} {measurementIngrident[i]}");
+                }
+                Console.WriteLine($"The steps for making {recipeName[0]}");
+                for (int i = 0; i < steps.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}.{steps[i]}");
+                }
+            }
+            catch (System.NullReferenceException)
+            {
+                Console.WriteLine("Sorry, you did not input any values");
+            }
+            finally
+            {
+                Menu_Options();
+            }
+        }
     }
 }
