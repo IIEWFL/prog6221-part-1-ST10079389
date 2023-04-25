1f8f465483754b20fec46b55e91dc821fbfead53
Kaushil Dajee <104414927+ST10079389@users.noreply.github.com>
2023-04-25 20:18:21 +0200

    Added classes

---
+++ ST10079389_Kaushil_Dajee_PROG6221/RecipeBook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10079389_Kaushil_Dajee_PROG6221
+{
+    internal class RecipeBook
+    {
+    }
+}
